72b36ffadd7079ba4da395603c67b1662bfc48ca
Tom Benbow <tom@hellomoon.net>
2023-04-27 13:41:16 -0500

    Fix theme page

--- pages/themes/[filename].tsx
+++ pages/themes/[filename].tsx
@@ -1,9 +1,38 @@
-import { Blocks } from "../components/blocks-renderer";
+import { client } from "../../.tina/__generated__/client";
 import { useTina } from "tinacms/dist/react";
-import { Layout } from "../components/layout";
-import { client } from "../.tina/__generated__/client";
+import { ThemeLayout } from "../../components/layout/theme-layout";
 
-export default function DynamicPage(
+function slugify(string: string) {
+  return string.replace(" ", "-").toLowerCase()
+}
+
+const Typography = ({ item, index, parentField = "" }) => {
+  const typography = JSON.parse(item.typography) || {}
+  let sample
+  if (Number(typography.size) > 40) {
+    sample = 'The quick brown fox jumps over the lazy dog'
+  } else if (Number(typography.size) > 20) {
+    sample = 'The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog'
+  } else {
+    sample = 'The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog'
+  }
+  
+  return (
+    <div className="flex items-center gap-10 mb-16">
+      <div className="flex-none text-gray text-right uppercase w-40">
+        <div style={{fontSize: "14px" }}>{item.label}</div>
+        <div style={{fontSize: "12px" }}>{typography.family}</div>
+        <div style={{fontSize: "12px" }}>{typography.size}px / {typography.lineHeight}px</div>
+      </div>
+      <div className=" w-full rounded overflow-hidden border-b border-gray-light">
+        <h3 className={item.label && `mg-${slugify(item.label)}`}>{sample}</h3>
+      </div>
+    </div>
+  )
+}
+
+// Use the props returned by get static props
+export default function ThemePage(
   props: AsyncReturnType<typeof getStaticProps>["props"]
 ) {
   const { data } = useTina({
@@ -11,54 +40,84 @@ export default function DynamicPage(
     variables: props.variables,
     data: props.data,
   });
-  const eventsData = props.events.eventConnection.edges
-  const eventList = eventsData.map(event => {
+  if (data) {
     return (
-      {
-        eventName: event.node?.eventName,
-        website: event.node?.website,
-        location: event.node?.location,
-        startDate: event.node?.startDate,
-        endDate: event.node?.endDate,
-        dateTBD: event.node?.dateTBD,
-        dri: event.node?.dri,
-        tag: event.node?.tag,
-      }
-    )
-  })
-
+      <ThemeLayout theme={data.theme}>
+        <div className="mx-auto max-w-desktop-full w-full p-10 py-20">
+          <div className="flex items-center gap-10 mb-10">
+            <div className="flex-none text-gray text-right uppercase w-40">
+              <div style={{fontSize: "14px" }}>Colors</div>
+            </div>
+            <div className="grid grid-cols-5 gap-4 w-full">
+              <div className="bg-primary rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-accent1 rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-accent2 rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-accent3 rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-accent4 rounded" style={{paddingBottom: "100%"}}></div>
+            </div>
+          </div>
+          <div className="flex items-center gap-10">
+            <div className="flex-none text-gray text-right uppercase w-40">
+              <div style={{fontSize: "14px" }}>Grays</div>
+            </div>
+            <div className="grid grid-cols-5 gap-4 w-full">
+              <div className="border-2 border-gray-light bg-white rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-gray-light rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-gray rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-gray-dark rounded" style={{paddingBottom: "100%"}}></div>
+              <div className="bg-black rounded" style={{paddingBottom: "100%"}}></div>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto max-w-desktop-full p-10">
+          {data.theme.typo &&
+            data.theme.typo.map(function (item, index) {
+              return <Typography key={index} index={index} item={item} />;
+            })
+          }
+        </div>
+        <div className="mx-auto max-w-desktop-full w-full p-10">
+          {data.theme.buttons &&
+            data.theme.buttons.map(function (item, index) {
+              return (
+                <div className="flex items-center gap-10 mb-16" key={index}>
+                  <div className="flex-none text-gray text-right uppercase w-40">
+                    <div style={{fontSize: "14px" }}>{item.label}</div>
+                  </div>
+                  <div className="w-full">
+                    <a className={item.label && `btn-${slugify(item.label)}`} href="#">Do Nothing</a>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </ThemeLayout>
+    );
+  }
   return (
-    /* TODO: needs ts type */
-    <Layout rawData={data}>
-      <Blocks {...data.page } events={eventList} />
-    </Layout>
+    <div>No data</div>
   );
 }
 
 export const getStaticProps = async ({ params }) => {
-  const tinaProps = await client.queries.contentQuery({
+  const tinaProps = await client.queries.themeQuery({
     relativePath: `${params.filename}.md`,
   });
-  const eventsListData = await client.queries.eventConnection();
   return {
     props: {
-      data: tinaProps.data,
-      query: tinaProps.query,
-      variables: tinaProps.variables,
-      events: eventsListData.data
+      ...tinaProps,
     },
   };
 };
 
 export const getStaticPaths = async () => {
-  const pagesListData = await client.queries.pageConnection();
-  const pageList = pagesListData.data.pageConnection.edges
-  const pageListFiltered = pageList.filter((page) => page.node._sys.filename !== "index")
+  const themesListData = await client.queries.themeConnection();
   return {
-    paths: pageListFiltered.map((page) => ({
-      params: { filename: page.node._sys.filename },
+    paths: themesListData.data.themeConnection.edges.map((theme) => ({
+      params: { filename: theme.node._sys.filename },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
